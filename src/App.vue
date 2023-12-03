3085e1e3f23dc8d816168d44b99fbad4b3392b50
checkcai <checkWork@126.com>
2023-12-03 15:57:03 +0800

    feat: add tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import { ConfigProvider } from 'ant-design-vue'
+import { ConfigProvider, StyleProvider } from 'ant-design-vue'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
 
 </script>
 
 <template>
   <ConfigProvider :locale="zhCN">
-    <RouterView />
+    <StyleProvider hash-priority="high">
+      <RouterView />
+    </StyleProvider>
   </ConfigProvider>
 </template>
